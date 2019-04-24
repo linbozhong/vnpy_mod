3f321792ee4d69169ac6a097c73712287d14f3fc
linbozhong <linbozhong@gmail.com>
2019-04-24 23:19:56 +0800

    新增功能

---
+++ userStrategy/omStrategy.py
@@ -0,0 +1,183 @@
+# encoding: UTF-8
+
+from vnpy.trader.vtConstant import (DIRECTION_LONG, DIRECTION_SHORT,
+                                    OFFSET_OPEN, OFFSET_CLOSE,
+                                    EMPTY_UNICODE)
+
+
+########################################################################
+class OmStrategyTemplate(object):
+    """策略模板"""
+    className = 'OmStrategyTemplate'
+    author = EMPTY_UNICODE
+    name = EMPTY_UNICODE  # 策略实例名称
+    vtSymbols = []  # 交易的合约vt系统代码
+
+    inited = False  # 是否进行了初始化
+    trading = False  # 是否启动交易，由引擎管理
+
+    # 参数列表，保存了参数的名称
+    paramList = ['name',
+                 'className',
+                 'author',
+                 'vtSymbols']
+
+    # 变量列表，保存了变量的名称
+    varList = ['inited',
+               'trading']
+
+    # ----------------------------------------------------------------------
+    def __init__(self, engine, setting):
+        """Constructor"""
+        self.engine = engine
+
+        self.vtSymbols = []
+
+        # 设置策略的参数
+        for key in self.paramList:
+            if key in setting:
+                self.__setattr__(key, setting[key])
+
+    # ----------------------------------------------------------------------
+    def onInit(self):
+        """初始化"""
+        raise NotImplementedError
+
+    # ----------------------------------------------------------------------
+    def onStart(self):
+        """启动"""
+        raise NotImplementedError
+
+    # ----------------------------------------------------------------------
+    def onStop(self):
+        """停止"""
+        raise NotImplementedError
+
+    # ----------------------------------------------------------------------
+    def onTick(self, tick):
+        """行情推送"""
+        raise NotImplementedError
+
+    # ----------------------------------------------------------------------
+    def onTrade(self, trade):
+        """成交推送"""
+        raise NotImplementedError
+
+    # ----------------------------------------------------------------------
+    def onOrder(self, order):
+        """委托推送"""
+        raise NotImplementedError
+
+    # ----------------------------------------------------------------------
+    def onTimer(self):
+        """定时推送"""
+        raise NotImplementedError
+
+        # ----------------------------------------------------------------------
+
+    def cancelOrder(self, vtOrderID):
+        """撤单"""
+        self.engine.cancelOrder(vtOrderID)
+
+    # ----------------------------------------------------------------------
+    def buy(self, vtSymbol, price, volume):
+        """开多"""
+        return self.engine.buy(vtSymbol, price, volume)
+
+    # ----------------------------------------------------------------------
+    def short(self, vtSymbol, price, volume):
+        """开空"""
+        return self.engine.short(vtSymbol, price, volume)
+
+        # ----------------------------------------------------------------------
+
+    def sell(self, vtSymbol, price, volume):
+        """平多"""
+        return self.engine.sell(vtSymbol, price, volume)
+
+        # ----------------------------------------------------------------------
+
+    def cover(self, vtSymbol, price, volume):
+        """平空"""
+        return self.engine.cover(vtSymbol, price, volume)
+
+        # ----------------------------------------------------------------------
+
+    def dbQuery(self, flt):
+        """查询数据"""
+        return self.engine.dbQuery(self.name, flt)
+
+    # ----------------------------------------------------------------------
+    def dbUpdate(self, d, flt):
+        """更新数据"""
+        return self.engine.dbUpdate(self.name, d, flt)
+
+    # ----------------------------------------------------------------------
+    def getOption(self, vtSymbol):
+        """查询期权数据"""
+        return self.engine.getOption(vtSymbol)
+
+    # ----------------------------------------------------------------------
+    def getUnderlying(self, vtSymbol):
+        """获取标的信息"""
+        return self.engine.getUnderlying(vtSymbol)
+
+    # ----------------------------------------------------------------------
+    def getChain(self, symbol):
+        """获取期权链信息"""
+        return self.engine.getChain(symbol)
+
+    # ----------------------------------------------------------------------
+    def getPortfolio(self):
+        """获取持仓组合信息"""
+        return self.engine.getPortfolio()
+
+    # ----------------------------------------------------------------------
+    def putEvent(self):
+        """发出GUI更新通知"""
+        self.engine.putStrategyEvent(self.name)
+
+    # ----------------------------------------------------------------------
+    def writeLog(self, content):
+        """记录日志"""
+        content = '%s:%s' % (self.name, content)
+        self.engine.writeLog(content)
+
+    # 自定义功能
+    # ----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
+
+    def addTickEvent(self, vtSymbol):
+        """把合约加入symbolStrategy列表，使得可以用onTick推送对应的行情"""
+        strategyList = self.engine.symbolStrategyDict.get(vtSymbol)
+        if strategyList is None:
+            self.engine.symbolStrategyDict[vtSymbol] = [self]
+        else:
+            if self not in strategyList:
+                strategyList.append(self)
+        print(strategyList)
+
+    def getAtmContract(self, chainSymbol):
+        """通过gamma值找到平值购和平值沽的合约代码"""
+        chain = self.getChain(chainSymbol)
+        callMaxGamma = 0
+        atmCallOptionSymbol = None
+        putMaxGamma = 0
+        atmPutOptionSymbol = None
+        for option in chain.callDict.values():
+            if option.theoGamma >= callMaxGamma:
+                callMaxGamma = option.theoGamma
+                atmCallOptionSymbol = option.vtSymbol
+            else:
+                break
+        for option in chain.putDict.values():
+            if option.theoGamma >= putMaxGamma:
+                putMaxGamma = option.theoGamma
+                atmPutOptionSymbol = option.vtSymbol
+            else:
+                break
+
+        self.addTickEvent(atmCallOptionSymbol)
+        self.addTickEvent(atmPutOptionSymbol)
+        return atmCallOptionSymbol, atmPutOptionSymbol
---
+++ userStrategy/omStrategyDemo.py
@@ -0,0 +1,84 @@
+# encoding: UTF-8
+
+# from vnpy.trader.app.optionMaster.omStrategy import OmStrategyTemplate
+
+# 改成载入本地目录的omStrategy模块，否则会引用安装目录的模块，从而引发动态引用找不到模块的错误
+from omStrategy import OmStrategyTemplate
+
+
+#######################################################################
+class DemoStrategy(OmStrategyTemplate):
+    """演示策略"""
+    className = 'DemoStrategy'
+    author = u'本地的用Python的交易员'
+
+    temp = 123
+
+    # 参数列表，保存了参数的名称
+    paramList = ['name',
+                 'className',
+                 'author',
+                 'vtSymbols',
+                 'temp',
+                 'chainSymbol']
+
+    # 变量列表，保存了变量的名称
+    varList = ['inited',
+               'trading']
+
+    # ----------------------------------------------------------------------
+    def __init__(self, engine, setting):
+        """Constructor"""
+        self.chainSymbol = ''
+
+        super(DemoStrategy, self).__init__(engine, setting)
+
+        self.timeInterval = 10
+        self.timeCount = 0
+
+    # ----------------------------------------------------------------------
+    def onInit(self):
+        """初始化"""
+        self.writeLog(u'%s策略初始化' % self.name)
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onStart(self):
+        """启动"""
+        self.writeLog(u'%s策略启动' % self.name)
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onStop(self):
+        """停止"""
+        self.writeLog(u'%s策略停止' % self.name)
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onTick(self, tick):
+        """行情推送"""
+        # self.writeLog(u'%s策略收到行情推送' % self.name)
+        self.writeLog(u'%s：最新价：%s' % (tick.symbol, tick.lastPrice))
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onTrade(self, trade):
+        """成交推送"""
+        self.writeLog(u'%s策略收到成交推送' % self.name)
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onOrder(self, order):
+        """委托推送"""
+        self.writeLog(u'%s策略收到委托推送' % self.name)
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onTimer(self):
+        """定时推送"""
+        self.timeCount += 1
+        if self.timeCount > self.timeInterval:
+            self.writeLog(u'%s策略收到定时推送，自定义参数%s' % (self.name, self.temp))
+            call, put = self.getAtmContract(self.chainSymbol)
+            self.writeLog(u'平值购:%s, 平值沽:%s' % (call, put))
+            self.timeCount = 0
