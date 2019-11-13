c841a7ad9301e84c0f9daf8c65c4e6dc68382e4d
linbozhong <linbozhong@gmail.com>
2019-11-13 20:50:47 +0800

    change directory

--- optionMaster/__init__.py
+++ optionMaster/__init__.py
@@ -1,10 +1,1 @@
-# encoding: UTF-8
-
-from .omEngine import OmEngine
-from .uiOmWidget import OmManager
-
-appName = 'OptionMaster'
-appDisplayName = u'OptionMaster'
-appEngine = OmEngine
-appWidget = OmManager
-appIco = 'om.ico'
+# coding:utf-8
