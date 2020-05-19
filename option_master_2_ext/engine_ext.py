1374463cdca5e08015a5a9f2f28bb764d58eac5e
linbozhong <linbozhong@gmail.com>
2020-05-19 23:16:30 +0800

    option master 2 engine_ext add chase order engine

--- option_master_2_ext/engine_ext.py
+++ option_master_2_ext/engine_ext.py
@@ -1,7 +1,17 @@
 import typing
+from typing import Optional, Dict, List, Set
 
-from typing import Optional, Dict, List
-from vnpy.event import Event, EventEngine, EVENT_TIMER
+
+from vnpy.event import Event, EventEngine
+from vnpy.trader.event import (
+    EVENT_TIMER, EVENT_ORDER
+)
+from vnpy.trader.constant import (
+    Status, Direction, Offset
+)
+from vnpy.trader.object import (
+    OrderData, OrderRequest, OrderType
+)
 from vnpy.trader.engine import BaseEngine, MainEngine
 from vnpy.app.option_master.engine import OptionEngine
 from vnpy.app.option_master.base import (
@@ -27,7 +37,8 @@ class ChannelHedgeEngine:
 
         self.balance_prices: Dict[str, float] = {}
         self.underlyings: Dict[str, UnderlyingData] = {}
-        self.synthesis_chains: Dict[str, str] = {}
+        self.underlying_symbols: Dict[str, str] = {}
+        self.synthesis_chain_symbols: Dict[str, str] = {}
 
         self.auto_portfolio_names: List[str] = []
         
@@ -65,6 +76,12 @@ class ChannelHedgeEngine:
         check_delta_counter += 1
         calc_balance_counter += 1
 
+    def set_underlying_symbol(self, portfolio_name: str, underlying_symbol: str):
+        self.underlying_symbols[portfolio_name] = underlying_symbol
+
+    def set_synthesis_chain_symbol(self, portfolio_name: str, chain_symbol: str):
+        self.synthesis_chain_symbols[portfolio_name] = chain_symbol
+
     def get_portfolio(self, portfolio_name: str) -> PortfolioData:
         active_portfolios = self.option_engine.active_portfolios
         portfolio = active_portfolios.get(portfolio_name)
@@ -80,7 +97,7 @@ class ChannelHedgeEngine:
             if not portfolio:
                 return
 
-            symbol = CHAIN_UNDERLYING_MAP.get(portfolio_name)
+            symbol = self.underlying_symbols.get(portfolio_name)
             if not symbol:
                 self.write_log(f"找不到组合{portfolio_name}对应标的代码")
                 return
@@ -103,7 +120,7 @@ class ChannelHedgeEngine:
         if not portfolio:
             return
 
-        chain_symbol = self.synthesis_chains.get(portfolio_name)
+        chain_symbol = self.synthesis_chain_symbols.get(portfolio_name)
         chain = portfolio.get_chain(chain_symbol)
         return chain
 
@@ -176,6 +193,20 @@ class ChannelHedgeEngine:
         atm_put = chain.puts[chain.atm_index]
         unit_hedge_delta = atm_call.cash_delta - atm_put.cash_delta
 
+        portfolio = self.get_portfolio(portfolio_name)
+        to_hedge_delta = abs(portfolio.pos_delta) * self.hedge_percent
+        to_hedge_volume = round(to_hedge_delta / unit_hedge_delta)
+
+        if not to_hedge_volume:
+            self.write_log(f"{portfolio_name} Delta偏移量少于最小对冲单元值")
+            return
+
+    def buy(self, volume: int):
+        pass
+
+    def short(self, volume: int):
+        pass
+
     def short_hedge(self, portfolio_name: str):
         pass
 
@@ -183,7 +214,89 @@ class ChannelHedgeEngine:
         self.main_engine.write_log(msg, source=APP_NAME)
 
 
+class ChaseOrderEngine:
+    def __init__(self, option_engine: OptionEngineExt):
+        self.option_engine: OptionEngineExt = option_engine
+        self.main_engine: MainEngine = option_engine.main_engine
+        self.event_engine: EventEngine = option_engine.event_engine
 
+        self.active_orderids: Set[str] = set()
+        
+        self.pay_up: int = 0
+        self.cancel_interval: int = 3
+        
+        self.cancel_counts: Dict[str, int] = {}
 
+        self.register_event()
 
+    def register_event(self) -> None:
+        self.event_engine.register(EVENT_ORDER, self.process_order_event)
+        self.event_engine.register(EVENT_TIMER, self.process_timer_event)
 
+    def process_order_event(self, event: Event) -> None:
+        order: OrderData = event.data
+
+        if order.vt_orderid not in self.active_orderids:
+            return
+
+        if not order.is_active():
+            self.active_orderids.remove(order.vt_orderid)
+            self.cancel_counts.pop(order.vt_orderid, None)
+
+        if order.status == Status.CANCELLED:
+            self.resend_order(order)
+
+    def process_timer_event(self, event: Event) -> None:
+        self.check_cancel()
+
+    def resend_order(self, order: OrderData) -> None:
+        new_volume = order.volume - order.traded
+        self.send_order(order.vt_symbol, order.direction, order.offset, new_volume)
+
+    def cancel_order(self, vt_orderid: str) -> None:
+        """"""
+        order = self.main_engine.get_order(vt_orderid)
+        req = order.create_cancel_request()
+        self.main_engine.cancel_order(req, order.gateway_name)
+
+    def check_cancel(self) -> None:
+        for vt_orderid in self.active_orderids:
+            if self.cancel_counts[vt_orderid] > self.cancel_interval:
+                self.cancel_order(vt_orderid)
+            self.cancel_counts[vt_orderid] += 1
+
+    def send_order(self, vt_symbol: str, direction: Direction, offset: Offset, volume: float, price: Optional[float] = None) -> str:
+        contract = self.main_engine.get_contract(vt_symbol)
+        tick = self.main_engine.get_tick(vt_symbol)
+
+        if not price:
+            if direction == Direction.LONG:
+                price = tick.ask_price_1 + contract.pricetick * self.pay_up
+            else:
+                price = tick.bid_price_1 - contract.pricetick * self.pay_up
+
+        req = OrderRequest(
+            symbol=contract.symbol,
+            exchange=contract.exchange,
+            direction=direction,
+            type=OrderType.LIMIT,
+            volume=volume,
+            price=price
+        )
+
+        vt_orderid = self.main_engine.send_order(req, contract.gateway_name)
+        self.active_orderids.add(vt_orderid)
+        self.cancel_counts[vt_orderid] = 0
+        return vt_orderid
+
+    def buy(self, vt_symbol: str, volume: float, price: Optional[float] = None):
+        return self.send_order(vt_symbol, Direction.LONG, Offset.OPEN, volume, price)
+
+    def sell(self, vt_symbol: str, volume: float, price: Optional[float] = None):
+        return self.send_order(vt_symbol, Direction.SHORT, Offset.CLOSE, volume, price)
+
+    def short(self, vt_symbol: str, volume: float, price: Optional[float] = None):
+        return self.send_order(vt_symbol, Direction.SHORT, Offset.OPEN, volume, price)
+
+    def cover(self, vt_symbol: str, volume: float, price: Optional[float] = None):
+        return self.send_order(vt_symbol, Direction.LONG, Offset.CLOSE, volume, price)
